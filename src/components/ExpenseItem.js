4f573f83394f0c799f3fb9e2c8e2bc24a7f3ea20
Uzair Iqbal <uzair.qq@outlook.com>
2022-11-14 20:42:53 +0500

    Passing Date Via "PROPS"

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
-  const date = new Date().toUTCString();
-  const expenseName = "Car Insurance";
-  const expenseCost = "129.23";
-
+const ExpenseItem = (props) => {
   return (
     <div className="expense-item">
-      <div>{date}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseName}</h2>
-        <div className="expense-item__price">{expenseCost}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
