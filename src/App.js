c110e720f2d9234cd0b5350dea870b1cfcb9aad6
Uzair Iqbal <uzair.qq@outlook.com>
2022-11-15 17:14:38 +0500

    Assignment No 1 , Creating Expenses Component and Clearing App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 const App = () => {
   const expenses = [
@@ -8,7 +8,12 @@ const App = () => {
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
     {
       id: "e3",
       title: "Car Insurance",
@@ -24,27 +29,8 @@ const App = () => {
   ];
   return (
     <div>
-      <h1>expense tracker app</h1>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <h1>Expense Tracker App</h1>
+      <Expenses expenses={expenses} />
     </div>
   );
 };
