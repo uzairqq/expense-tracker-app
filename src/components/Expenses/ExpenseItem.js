c119afe63559f27aec682fa2c96b813dd38c3ca0
Uzair Iqbal <uzair.qq@outlook.com>
2022-11-18 19:10:17 +0500

    Using Events

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,6 +2,9 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log("Clicked");
+  };
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
@@ -9,7 +12,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">{"$" + props.amount}</div>
       </div>
-      <button onClick={() => console.log("Clicked!!!")}>Try Me!!</button>
+      <button onClick={clickHandler}>Try Me!!</button>
     </div>
   );
 };
