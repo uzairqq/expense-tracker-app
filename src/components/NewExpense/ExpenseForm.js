8ff947de1968a6a43c9e21e32cbdb6da1e8eb2dc
Uzair Iqbal <uzair.qq@outlook.com>
2022-11-21 16:25:42 +0500

    Using Multiple States in One Component

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
   const handleTitleChange = (e) => {
+    setEnteredTitle(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const handleDateChane = (e) => {
+    setEnteredDate(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const handleAmountChange = (e) => {
     console.log(e.target.value);
   };
 
@@ -14,11 +28,16 @@ const ExpenseForm = () => {
         </div>
         <div className="new-expense__control">
           <label htmlFor="">Amount</label>
-          <input type="number" min={0.01} />
+          <input type="number" min={0.01} onChange={handleAmountChange} />
         </div>
         <div className="new-expense__control">
           <label htmlFor="">Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={handleDateChane}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
