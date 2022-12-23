6fdfd1e6cf14a79ef3149c8570ea99b127cecb7f
Uzair Iqbal <uzair.qq@outlook.com>
2022-12-23 22:52:50 +0500

    Added More Deeper Component Expense List In Expenses.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,37 +1,26 @@
 import { useState } from "react";
 import ExpenseFilter from "../ExpenseFilter/ExpenseFilter";
-import ExpenseItem from "./ExpenseItem";
+import ExpenseList from "./ExpenseList";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-  const [year, setSelectedYear] = useState("2021");
+  const [year, setSelectedYear] = useState("all");
   const handleSelectedYear = (year) => {
     setSelectedYear(year);
   };
 
   const filteredExpenses = props.expenses.filter((expense) => {
+    if (year === "all") {
+      return expense;
+    }
     return expense.date.getFullYear().toString() === year;
   });
-
-  let expenseContent = "";
-  if (filteredExpenses.length === 0) {
-    expenseContent = <p class="NoExpense">No Expenses Found</p>;
-  } else {
-    expenseContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
 
   return (
     <div className="expenses">
       <div>
         <ExpenseFilter selected={year} onYearSelected={handleSelectedYear} />
-        {expenseContent}
+        <ExpenseList filteredExpense={filteredExpenses} />
       </div>
     </div>
   );
