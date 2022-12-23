36a2d7901f00226758ea540582497ad705fb1d6a
Uzair Iqbal <uzair.qq@outlook.com>
2022-12-23 19:20:26 +0500

    Second Approach Of Conditional Content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,23 +13,26 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === year;
   });
 
+  let expenseContent = "";
+  if (filteredExpenses.length === 0) {
+    expenseContent = <p class="NoExpense">No Expenses Found</p>;
+  } else {
+    expenseContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <div className="expenses">
       <div>
         <ExpenseFilter selected={year} onYearSelected={handleSelectedYear} />
+        {expenseContent}
       </div>
-      {filteredExpenses.length == 0 ? (
-        <p class="NoExpense">No Expenses Found</p>
-      ) : (
-        filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))
-      )}
     </div>
   );
 };
