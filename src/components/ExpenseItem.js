612435347767516693fcb5b3fc05d6edb34148d7
Uzair Iqbal <uzair.qq@outlook.com>
2023-06-10 21:51:34 +0500

    Added Basic Styling to Expense Item Component.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,12 @@
+import "./ExpenseItem.css";
 const ExpenseItem = () => {
   return (
-    <div>
-      <h1>Expense Item Component</h1>
+    <div className="expense-item">
+      <h1>March 28th 2023</h1>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$294.67</div>
+      </div>
     </div>
   );
 };
