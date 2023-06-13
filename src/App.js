efa5624d660f48ff77c5a22c021cebe31710b354
Uzair Iqbal <uzair.qq@outlook.com>
2023-06-14 03:35:11 +0500

    Parsing Full Date in small chunks

--- src/App.js
+++ src/App.js
@@ -7,25 +7,25 @@ function App() {
       id: "e1",
       title: "Toilet Paper",
       amount: 94.12,
-      date: new Date(2020, 7, 14).toDateString(),
+      date: new Date(2020, 7, 14),
     },
     {
       id: "e2",
       title: "New TV",
       amount: 799.49,
-      date: new Date(2021, 2, 12).toDateString(),
+      date: new Date(2021, 2, 12),
     },
     {
       id: "e3",
       title: "Car Insurance",
       amount: 294.67,
-      date: new Date(2021, 2, 28).toDateString(),
+      date: new Date(2021, 2, 28),
     },
     {
       id: "e4",
       title: "New Desk (Wooden)",
       amount: 450,
-      date: new Date(2021, 5, 12).toDateString(),
+      date: new Date(2021, 5, 12),
     },
   ];
   return (
@@ -36,7 +36,7 @@ function App() {
         amount={expenses[0].amount}
         date={expenses[0].date}
       />
-      <ExpenseItem
+      {/* <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
@@ -50,7 +50,7 @@ function App() {
         title={expenses[3].title}
         amount={expenses[3].amount}
         date={expenses[3].date}
-      />
+      /> */}
     </div>
   );
 }
